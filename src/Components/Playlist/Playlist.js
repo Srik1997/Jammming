fb4ab027504979083368b72f902629a52135e64e
Srikanth S V <90049777+Srik1997@users.noreply.github.com>
2021-11-20 13:57:07 +0530

    Add files via upload

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import './Playlist.css';
+
+import TrackList from "../TrackList/TrackList";
+
+/*class Playlist extends React.Component {
+    render() {
+        return (
+            <div class="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                {<TrackList />}
+                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
+export default Playlist;*/
+class Playlist extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.handleNameChange = this.handleNameChange.bind(this);
+    }
+  
+    handleNameChange(event) {
+      this.props.onNameChange(event.target.value);
+    }
+  
+    render() {
+      return (
+        <div className="Playlist">
+          <input onChange={this.handleNameChange} defaultValue={'New Playlist'} />
+          <TrackList tracks={this.props.playlistTracks}
+                     isRemoval={true}
+                     onRemove={this.props.onRemove} />
+          <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+        </div>
+      );
+    }
+  }
+  
+  export default Playlist;
